ec9c095a572f2bf0a840d0642ac8226987b8beac
Sandmali <sandamalitangalle18@gmail.com>
2020-01-13 15:42:22 +0530

    First Commit

---
+++ ReadWithUsLibManSystem/src/readwithuslibmansystem/ReadWithUsLibManSystem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package readwithuslibmansystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author abans tangalle
+ */
+public class ReadWithUsLibManSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login log = new Login();
+        log.setVisible(true);
+        
+    }
+    
+   
+}
---
+++ ReadWithUsLibManSystem/src/readwithuslibmansystem/UserType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package readwithuslibmansystem;
+
+/**
+ *
+ * @author abans tangalle
+ */
+public class UserType {
+    
+    public static String usertype = "";
+    
+}
